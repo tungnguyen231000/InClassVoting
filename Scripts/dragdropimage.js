19f1d181053f2600c9f8e6632a5b72f47ee11c38
tungnguyen231000 <tung.nguyen.1023@gmail.com>
2021-11-06 17:18:43 +0700

    ok

---
+++ Scripts/dragdropimage.js
@@ -0,0 +1,52 @@
+const dragArea = document.querySelector(".drag-area");
+const header = document.querySelector(".header"),
+	  btn = document.querySelector(".btn"),
+	  input = document.querySelector(".file-input");
+
+let file;
+
+
+btn.onclick = ()=>{
+	input.click();
+};
+
+input.addEventListener("change", function(){
+	file = this.files[0];
+	show(file);
+});
+
+
+// di chuyển bên trong
+dragArea.addEventListener("dragover", (event)=>{
+	event.preventDefault();
+	dragArea.classList.add('active');
+	header.textContent = 'Release to Upload File';
+});
+
+// di chuyển bên ngoài
+dragArea.addEventListener("dragleave", ()=>{
+	dragArea.classList.remove('active');
+	header.textContent = 'Drag and Drop to Upload File';
+});
+
+// thả bên trong
+dragArea.addEventListener("drop", (event)=>{
+	event.preventDefault();
+	file = event.dataTransfer.files[0];
+	show(file);
+});
+
+function show(file){
+	let validImage = ['image/jpeg','image/jpg','image/png'];
+	if(validImage.includes(file.type)){
+		let fileReader = new FileReader();
+		fileReader.onload = ()=>{
+			let fileURL = fileReader.result;
+			let imgTag = '<img src="'+fileURL+'" alt="">';
+			dragArea.innerHTML=imgTag;
+		}
+		fileReader.readAsDataURL(file);
+	}else{
+		alert('This is not a Image File');
+	}
+}
---
+++ Scripts/validate.js
@@ -0,0 +1,201 @@
+﻿$(document).ready(function () {
+	console.log('validate');
+	$("#fillblank-form").validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		rules: {
+			"time": {
+				required: true
+			},
+			"mark": {
+				required: true
+			},
+			"question": {
+				required: true
+			},
+			"answer": {
+				required: true
+			}
+		},
+		messages: {
+			"time": {
+				required: "*Time is required"
+			},
+			"mark": {
+				required: "*Mark is required"
+			},
+			"question": {
+				required: "*Question is required"
+			},
+			"answer": {
+				required: "*Answer is required"
+			}
+		}
+	});
+
+	$('#checkbox-form').validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		groups: {
+			nameGroup: "cbOption option"
+		},
+		rules: {
+			"time": {
+				required: true
+			},
+			"mark": {
+				required: true
+			},
+			"questionText": {
+				required: true
+			},
+			"cbOption": {
+				required: true
+			},
+			"option": {
+				required: true
+			}
+		},
+		messages: {
+			"time": {
+				required: "*Time is required"
+			},
+			"mark": {
+				required: "*Mark is required"
+			},
+			"questionText": {
+				required: "*Question is required"
+			},
+			"cbOption": {
+				required: "*Checkbox is required"
+			},
+			"option": {
+				required: "*Option is required"
+			}
+		},
+		errorPlacement: function (error, element) {
+			if (element.attr("name") == "cbOption" || element.attr("name") == "option") {
+				error.insertAfter(".ms-error");
+			} else {
+				error.insertAfter(element);
+			}
+		}
+
+	});
+
+	$('#reading-form').validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		groups: {
+			nameGroup: "cb-option option"
+		},
+		rules: {
+			"time": {
+				required: true
+			},
+			"mark": {
+				required: true
+			},
+			"paragraph": {
+				required: true
+			},
+			"question": {
+				required: true
+			},
+			"cb-option": {
+				required: true
+			},
+			"option": {
+				required: true
+			}
+		},
+		messages: {
+			"time": {
+				required: "*Time is required"
+			},
+			"mark": {
+				required: "*Mark is required"
+			},
+			"paragraph": {
+				required: "*Paragraph is required"
+			},
+			"question": {
+				required: "*Question is required"
+			},
+			"cb-option": {
+				required: "*Checkbox is required"
+			},
+			"option": {
+				required: "*Option is required"
+			}
+		},
+		errorPlacement: function (error, element) {
+			if (element.attr("name") == "cb-option" || element.attr("name") == "option") {
+				error.insertAfter(".ms-error");
+			} else {
+				error.insertAfter(element);
+			}
+		}
+
+	});
+
+	$("#matching-form").validate({
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false,
+		groups: {
+			nameGroup: "txt-left txt-right"
+		},
+		rules: {
+			"time": {
+				required: true
+			},
+			"mark": {
+				required: true
+			},
+			"matching-left": {
+				required: true
+			},
+			"matching-right": {
+				required: true
+			},
+			"txt-left": {
+				required: true
+			},
+			"txt-right": {
+				required: true
+			}
+		},
+		messages: {
+			"time": {
+				required: "*Time is required"
+			},
+			"mark": {
+				required: "*Mark is required"
+			},
+			"matching-left": {
+				required: "*Matching question is required"
+			},
+			"matching-right": {
+				required: "*Matching question is required"
+			},
+			"txt-left": {
+				required: "*Option left is required"
+			},
+			"txt-right": {
+				required: "*Option right is required"
+			},
+		},
+		errorPlacement: function (error, element) {
+			if (element.attr("name") == "txt-left" || element.attr("name") == "txt-right") {
+				error.insertAfter(".ms-error");
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
+
+});
